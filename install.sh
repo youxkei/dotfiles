b936e237131d2ab8bd1c464910ccedd9cc0a3a2a
youxkei <youxkei@gmail.com>
2019-01-22 20:15:35 +0900

    [i3] Add workspace to monitor assignment settings

--- install.sh
+++ install.sh
@@ -8,29 +8,14 @@ declare -a DOTFILES=(
   .gitconfig
   .gitignore
   .i3status.conf
-  .latexmkrc
   .profile
   .tigrc
   .zshrc
 )
 
-# Check whether the $DOTFILES exist in $SCRIPT_DIR
-for dotfile in ${DOTFILES[@]}; do
-  if [[ ! -e $dotfile ]]; then
-    echo "$dotfile does not exist in $SCRIPT_DIR" >&2
-    exit 1
-  fi
-done
-
-# Create symlinks for $DOTFILES
-for dotfile in ${DOTFILES[@]}; do
-  # Remove existing file, directory, or link
-  if [[ -e ~/$dotfile ]]; then
-    rm -rf ~/$dotfile
-  fi
-
-  ln -s $SCRIPT_DIR/$dotfile ~
-done
+declare -a TEMPLATE_FILES=(
+  .Xresources.template
+)
 
 declare -a CONFIG_DIRS=(
   fontconfig
@@ -38,20 +23,52 @@ declare -a CONFIG_DIRS=(
   nvim
 )
 
-# Check whether the $CONFIG_DIRS exist in $SCRIPT_DIR
-for config_dir in ${CONFIG_DIRS[@]}; do
-  if [[ ! -d $config_dir ]]; then
-    echo "$config_dir does not exist in $SCRIPT_DIR" >&2
+
+# Check whether the $DOTFILES, $TEMPLATE_FILES, and $CONFIG_DIRS exist in $SCRIPT_DIR
+for dotfile in ${DOTFILES[@]} ${TEMPLATE_FILES[@]} ${CONFIG_DIRS[@]}; do
+  if [[ ! -e $dotfile ]]; then
+    echo "$dotfile does not exist in $SCRIPT_DIR" >&2
     exit 1
   fi
 done
 
+
+# Create symlinks for $DOTFILES
+for dotfile in ${DOTFILES[@]}; do
+  # Remove existing file, directory, or link
+  if [[ -e $HOME/$dotfile ]]; then
+    rm -rf $HOME/$dotfile
+  fi
+
+  ln -s $SCRIPT_DIR/$dotfile $HOME
+done
+
+# Copy $TEMPLATE_FILES to $HOME
+for template_file in ${TEMPLATE_FILES[@]}; do
+  dotfile=${template_file%.template}
+
+  # Remove existing file, directory, or link
+  if [[ -e $HOME/$dotfile ]]; then
+    rm -rf $HOME/$dotfile
+  fi
+
+  cp $SCRIPT_DIR/$template_file $HOME/$dotfile
+done
+
+
 # Create symlinks for $CONFIG_DIRS
 for config_dir in ${CONFIG_DIRS[@]}; do
   # Remove existing file, directory, or link
-  if [[ -e ~/.config/$config_dir ]]; then
-    rm -rf ~/.config/$config_dir
+  if [[ -e $HOME/.config/$config_dir ]]; then
+    rm -rf $HOME/.config/$config_dir
   fi
 
-  ln -s $SCRIPT_DIR/$config_dir ~/.config
+  ln -s $SCRIPT_DIR/$config_dir $HOME/.config
+done
+
+echo "Plese edit following config files for this host:"
+for template_file in ${TEMPLATE_FILES[@]}; do
+  dotfile=${template_file%.template}
+
+  echo $HOME/$dotfile
 done
