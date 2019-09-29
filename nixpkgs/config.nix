04f7d9583976457bedda1023873861b945544958
youxkei <youxkei@gmail.com>
2019-09-30 02:55:41 +0900

    [nix] Use i3-gaps from nixpkgs

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,20 +1,10 @@
 {
   allowUnfree = true;
-  packageOverrides = super: with super; let
-    modified-i3-gaps = i3-gaps.overrideAttrs (oldAttrs : rec {
-      version = "4.16.1";
-      releaseDate = "2019-01-28";
-
-      src = fetchurl {
-        url = "https://github.com/Airblader/i3/archive/${version}.tar.gz";
-        sha256 = "1jvyd8p8dfsidfy2yy7adydynzvaf72lx67x71r13hrk8w77hp0k";
-      };
-    });
-  in {
+  packageOverrides = super: with super; {
     all = buildEnv {
       name = "all";
       paths = [
-        modified-i3-gaps
+        i3-gaps
         i3status
         rofi
         feh
