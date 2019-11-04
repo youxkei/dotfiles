4b20387b9e4beb6ca9f5b0a376449398519144a8
youxkei <youxkei@gmail.com>
2019-11-05 04:49:00 +0900

    [nix] Introduce nix-npm-install

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,6 +1,17 @@
 {
   allowUnfree = true;
-  packageOverrides = super: with super; {
+  packageOverrides = super: with super; rec {
+    nix-npm-install = pkgs.writeScriptBin "nix-npm-install" ''
+      #!/usr/bin/env bash
+      tempdir="/tmp/nix-npm-install/$1"
+      mkdir -p $tempdir
+      pushd $tempdir
+      # note the differences here:
+      ${nodePackages.node2nix}/bin/node2nix --nodejs-12 --input <( echo "[\"$1\"]")
+      nix-env --install --file .
+      popd
+    '';
+
     all = buildEnv {
       name = "all";
       paths = [
@@ -41,6 +52,9 @@
         erlangR18
         rustup
         opam
+
+        nodejs
+        nix-npm-install
 
         go
         dep
