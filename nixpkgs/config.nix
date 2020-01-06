5718464ceccc72b29997ea8a892d26915dbd7442
youxkei <youxkei@gmail.com>
2020-01-07 03:08:48 +0900

    [nix] Add callPackage function

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,66 +1,77 @@
 {
   allowUnfree = true;
-  packageOverrides = super: with super; rec {
-    nix-npm-install = pkgs.writeScriptBin "nix-npm-install" ''
-      #!/usr/bin/env bash
-      tempdir="/tmp/nix-npm-install/$1"
-      mkdir -p $tempdir
-      pushd $tempdir
-      # note the differences here:
-      ${nodePackages.node2nix}/bin/node2nix --nodejs-12 --input <( echo "[\"$1\"]")
-      nix-env --install --file .
-      popd
+  packageOverrides = nixpkgs: let
+    nix-npm-install = nixpkgs.writeScriptBin "nix-npm-install" ''
+          #!/usr/bin/env bash
+          tempdir="/tmp/nix-npm-install/$1"
+          mkdir -p $tempdir
+          pushd $tempdir
+          # note the differences here:
+          ${nixpkgs.nodePackages.node2nix}/bin/node2nix --nodejs-12 --input <( echo "[\"$1\"]")
+          nix-env --install --file .
+          popd
     '';
 
-    all = buildEnv {
-      name = "all";
-      paths = [
-        i3-gaps
-        i3status
-        rofi
-        feh
-        glibcLocales
+    allPackages = nixpkgs // packages;
+    callPackage = nixpkgs.lib.callPackageWith allPackages;
 
-        gitAndTools.gitFull
-        gitAndTools.hub
-        gitAndTools.tig
-        gitAndTools.diff-so-fancy
-        rlwrap
-        trash-cli
-        neovim
-        tokei
-        xsel
-        ripgrep
-        fd
-        exa
-        gron
-        translate-shell
-        powertop
-        figlet
-        fzf
-        jq
-        yq
-        jo
-        jid
-        direnv
-        unar
-        httpie
-        universal-ctags
+    packages = rec {
+      all = nixpkgs.buildEnv {
+        name = "all";
+        paths = with nixpkgs; [
+          i3-gaps
+          i3status
+          rofi
+          feh
+          glibcLocales
 
-        gcc
+          gitAndTools.gitFull
+          gitAndTools.hub
+          gitAndTools.tig
+          gitAndTools.diff-so-fancy
+          rlwrap
+          trash-cli
+          neovim
+          tokei
+          xsel
+          ripgrep
+          fd
+          exa
+          gron
+          translate-shell
+          powertop
+          figlet
+          fzf
+          jq
+          yq
+          jo
+          jid
+          direnv
+          unar
+          httpie
+          sysstat
+          universal-ctags
+          progress
 
-        erlangR18
-        rustup
-        opam
+          gcc
 
-        nodejs
-        nix-npm-install
+          erlangR18
 
-        go
-        dep
+          rustup
 
-        ldc
-      ];
+          opam
+
+          nodejs
+          nix-npm-install
+
+          go
+          dep
+
+          ldc
+
+          ruby
+        ];
+      };
     };
-  };
+  in packages;
 }
