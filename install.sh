edb9036709f156aacb09cf378b0faee03ee76d51
youxkei <youxkei@gmail.com>
2020-08-15 20:08:13 +0900

    [i3][jack] Introduce jack and refine install.sh

--- install.sh
+++ install.sh
@@ -2,74 +2,81 @@
 set -eu pipefail
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-declare -a DOTFILES=(
-  .profile
-  .zprofile
+# source path (relative to $SCRIPT_DIR) -> destination path (relative to home directory)
+declare -A PATHS=(
+    [.profile]=.profile
+    [.zprofile]=.zprofile
+
+    [zsh]=.config/zsh
+    [fontconfig]=.config/fontconfig
+    [nixpkgs]=.config/nixpkgs
+    [nvim]=.config/nvim
+    [i3]=.config/i3
+    [i3status-rust]=.config/i3status-rust
+    [git]=.config/git
+    [tig]=.config/tig
+    [tmux]=.config/tmux
+    [procs]=.config/procs
+    [alacritty]=.config/alacritty
+    [compton.conf]=.config/compton.conf
+    [starship.toml]=.config/starship.toml
+
+    [jack/start_jack.sh]=bin/start_jack.sh
+    [i3/i3_move.sh]=bin/i3_move.sh
 )
 
-declare -a TEMPLATE_FILES=(
-  .Xresources.template
+# source path (relative to $SCRIPT_DIR) -> destination path (relative to home directory)
+declare -A TEMPLATE_PATHS=(
+    [.Xresources.template]=.Xresources
 )
 
-declare -a CONFIG_PATHS=(
-  zsh
-  fontconfig
-  nixpkgs
-  nvim
-  i3
-  i3status-rust
-  git
-  tig
-  tmux
-  procs
-  alacritty
-  compton.conf
-  starship.toml
-)
-
-
-# Check whether the $DOTFILES, $TEMPLATE_FILES, and $CONFIG_PATHS exist in $SCRIPT_DIR
-for dotfile in ${DOTFILES[@]} ${TEMPLATE_FILES[@]} ${CONFIG_PATHS[@]}; do
-  if [[ ! -e $dotfile ]]; then
-    echo "$dotfile does not exist in $SCRIPT_DIR" >&2
-    exit 1
-  fi
+# Check whether the source paths exist in $SCRIPT_DIR
+for path in ${!PATHS[@]}; do
+    if [[ ! -e $SCRIPT_DIR/$path ]]; then
+        echo "Error: $path does not exist in $SCRIPT_DIR" >&2
+        exit 1
+    fi
 done
 
+# Create symlinks for $PATHS
+for path in ${!PATHS[@]}; do
+    src=$SCRIPT_DIR/$path
+    dst=~/${PATHS[$path]}
 
-# Create symlinks for $DOTFILES
-for dotfile in ${DOTFILES[@]}; do
-  # Remove existing file, directory, or link
-  if [[ -e $HOME/$dotfile ]]; then
-    rm -rf $HOME/$dotfile
-  fi
+    # Remove links
+    if [[ -e $dst ]]; then
+        if [[ -L $dst ]]; then
+            echo "Link $dst -> $src"
+            unlink $dst
+            ln -s $src $dst
+        else
+            echo "Error: $dst exists and is not a symlink" >&2
+            exit 1
+        fi
+    else
+        if [[ ! -d $(dirname $dst) ]]; then
+            mkdir -p $(dirname $dst)
+        fi
 
-  ln -s $SCRIPT_DIR/$dotfile $HOME
+        echo "Link $dst -> $src"
+        ln -s $src $dst
+    fi
 done
 
 # Copy $TEMPLATE_FILES to $HOME
-for template_file in ${TEMPLATE_FILES[@]}; do
-  dotfile=${template_file%.template}
+for template_path in ${!TEMPLATE_PATHS[@]}; do
+    src=$SCRIPT_DIR/$template_path
+    dst=~/${TEMPLATE_PATHS[$template_path]}
 
-  if [[ ! -e $HOME/$dotfile ]]; then
-      cp $SCRIPT_DIR/$template_file $HOME/$dotfile
-  fi
+    if [[ -e $dst ]]; then
+        echo "$dst already exists. Skip copying"
+    else
+        echo "Copy $src -> $dst"
+        cp $src $dst
+    fi
 done
 
-
-# Create symlinks for $CONFIG_PATHS
-for config_path in ${CONFIG_PATHS[@]}; do
-  # Remove existing file, directory, or link
-  if [[ -e $HOME/.config/$config_path ]]; then
-    rm -rf $HOME/.config/$config_path
-  fi
-
-  ln -s $SCRIPT_DIR/$config_path $HOME/.config
-done
-
-echo "Plese edit following config files for this host:"
-for template_file in ${TEMPLATE_FILES[@]}; do
-  dotfile=${template_file%.template}
-
-  echo $HOME/$dotfile
+echo "Please edit following config files for this host:"
+for template_path in ${!TEMPLATE_PATHS[@]}; do
+    echo ~/${TEMPLATE_PATHS[$template_path]}
 done
---
+++ jack/start_jack.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+jack_control start
+sleep 1s
+jack_disconnect 'PulseAudio JACK Sink:front-left' system:playback_1
+jack_disconnect 'PulseAudio JACK Sink:front-right' system:playback_2
+jack_disconnect system:capture_1 'PulseAudio JACK Source:front-left'
+jack_disconnect system:capture_2 'PulseAudio JACK Source:front-right'
+
+pacmd load-module module-virtual-surround-sink sink_name=surround sink_properties=device.description=surround master=jack_out hrir=$HOME/repo/dotfiles/jack/hrir-1003.wav
+pacmd set-default-sink surround
+pacmd set-default-source jack_in
+
+carla ~/repo/dotfiles/jack/patch.carxp
