40e083e81d92dd936bdc20527bdf71893736fb74
youxkei <youxkei@gmail.com>
2020-10-01 19:47:19 +0900

    Use absolute destination paths in install.sh and introduce talk command

--- install.sh
+++ install.sh
@@ -2,32 +2,33 @@
 set -eu pipefail
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-# source path (relative to $SCRIPT_DIR) -> destination path (relative to home directory)
+# source path (relative to $SCRIPT_DIR) -> destination path
 declare -A PATHS=(
-    [.profile]=.profile
-    [.zprofile]=.zprofile
+    [.profile]=~/.profile
+    [.zprofile]=~/.zprofile
 
-    [zsh]=.config/zsh
-    [fontconfig]=.config/fontconfig
-    [nixpkgs]=.config/nixpkgs
-    [nvim]=.config/nvim
-    [i3]=.config/i3
-    [i3status-rust]=.config/i3status-rust
-    [git]=.config/git
-    [tig]=.config/tig
-    [tmux]=.config/tmux
-    [procs]=.config/procs
-    [alacritty]=.config/alacritty
-    [compton.conf]=.config/compton.conf
-    [starship.toml]=.config/starship.toml
+    [zsh]=$XDG_CONFIG_HOME/zsh
+    [fontconfig]=$XDG_CONFIG_HOME/fontconfig
+    [nixpkgs]=$XDG_CONFIG_HOME/nixpkgs
+    [nvim]=$XDG_CONFIG_HOME/nvim
+    [i3]=$XDG_CONFIG_HOME/i3
+    [i3status-rust]=$XDG_CONFIG_HOME/i3status-rust
+    [git]=$XDG_CONFIG_HOME/git
+    [tig]=$XDG_CONFIG_HOME/tig
+    [tmux]=$XDG_CONFIG_HOME/tmux
+    [procs]=$XDG_CONFIG_HOME/procs
+    [alacritty]=$XDG_CONFIG_HOME/alacritty
+    [compton.conf]=$XDG_CONFIG_HOME/compton.conf
+    [starship.toml]=$XDG_CONFIG_HOME/starship.toml
 
-    [jack/start_jack.sh]=bin/start_jack.sh
-    [i3/i3_move.sh]=bin/i3_move.sh
+    [jack/start_jack.sh]=~/bin/start_jack.sh
+    [i3/i3_move.sh]=~/bin/i3_move.sh
+    [bin/talk]=~/bin/talk
 )
 
-# source path (relative to $SCRIPT_DIR) -> destination path (relative to home directory)
+# source path (relative to $SCRIPT_DIR) -> destination path
 declare -A TEMPLATE_PATHS=(
-    [.Xresources.template]=.Xresources
+    [.Xresources.template]=~/.Xresources
 )
 
 # Check whether the source paths exist in $SCRIPT_DIR
@@ -41,9 +42,8 @@ done
 # Create symlinks for $PATHS
 for path in ${!PATHS[@]}; do
     src=$SCRIPT_DIR/$path
-    dst=~/${PATHS[$path]}
+    dst=${PATHS[$path]}
 
-    # Remove links
     if [[ -e $dst ]]; then
         if [[ -L $dst ]]; then
             echo "Link $dst -> $src"
@@ -66,7 +66,7 @@ done
 # Copy $TEMPLATE_FILES to $HOME
 for template_path in ${!TEMPLATE_PATHS[@]}; do
     src=$SCRIPT_DIR/$template_path
-    dst=~/${TEMPLATE_PATHS[$template_path]}
+    dst=${TEMPLATE_PATHS[$template_path]}
 
     if [[ -e $dst ]]; then
         echo "$dst already exists. Skip copying"
@@ -78,5 +78,5 @@ done
 
 echo "Please edit following config files for this host:"
 for template_path in ${!TEMPLATE_PATHS[@]}; do
-    echo ~/${TEMPLATE_PATHS[$template_path]}
+    echo ${TEMPLATE_PATHS[$template_path]}
 done
