b3fbd288e9d6363460e5f565195b9503d20259d5
youxkei <youxkei@gmail.com>
2020-12-14 19:33:23 +0900

    [jack] Add extra jack source for pulseaudio

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -8,7 +8,10 @@ jack_disconnect system:capture_2 'PulseAudio JACK Source:front-right'
 
 pacmd unload-module module-virtual-surround-sink
 pacmd load-module module-virtual-surround-sink sink_name=surround sink_properties=device.description=surround master=jack_out hrir=$HOME/repo/dotfiles/jack/hrir-1003.wav
+
+pacmd load-module module-jack-source client_name=PulseAudio_JACK_Source_2 connect=no
+
 pacmd set-default-sink surround
 pacmd set-default-source jack_in
 
-chrt 95 carla ~/repo/dotfiles/jack/patch.carxp
+carla ~/repo/dotfiles/jack/patch.carxp
