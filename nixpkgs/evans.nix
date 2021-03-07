5fab25e2b1b24c41cd39fa7147d24e0dc480958f
youxkei <youxkei@gmail.com>
2021-03-07 23:11:16 +0900

    [nix] Update evans

--- nixpkgs/evans.nix
+++ nixpkgs/evans.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "evans";
-  version = "0.9.1";
+  version = "0.9.3";
 
   src = fetchFromGitHub {
     owner = "ktr0731";
     repo = pname;
     rev = version;
-    sha256 = "0nyzgaw3igki6scxsxmjv0wb2m1fjz58fz7nfa5y1w0vwjs0fhvy";
+    sha256 = "1jy73737ah0qqy16ampx51cchrjrhkr945lpfnnshnalk86xdhdb";
   };
 
   doCheck = false;
 
-  vendorSha256 = "11yvsfj3kkwm0z17695rmycvgdgfrc8dnd41r1z0430zv6x67b9f";
+  vendorSha256 = "1y98alg153p2djn7zxvpyiprdzpra2nxyga8qjark38hpfnnbm4y";
 }
