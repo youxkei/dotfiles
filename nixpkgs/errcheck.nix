f56c8b993537b791ab186b4930537380d28573b8
youxkei <youxkei@gmail.com>
2021-03-07 23:11:05 +0900

    [nix] Update errcheck

--- nixpkgs/errcheck.nix
+++ nixpkgs/errcheck.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "errcheck";
-  version = "1.4.0";
+  version = "1.6.0";
 
   src = fetchFromGitHub {
     owner = "kisielk";
     repo = pname;
     rev = "v${version}";
-    sha256 = "00skyvy31yliw0f395j5h3gichi5n2q1m24izjidxvyc2av7pjn6";
+    sha256 = "17b1khy6lwy39qqkw0xj4s4wf58azd03kj5fa9jdf553rpcxzg1y";
   };
 
   vendorSha256 = "0mx506qb5sy6p4zqjs1n0w7dg8pz2wf982qi9v7nrhxysl2rlnxf";
