f79d3a361a2ca4defa9bb9b963ab150339f9e00a
youxkei <youxkei@gmail.com>
2021-03-07 23:11:46 +0900

    [nix] Update xcp

--- nixpkgs/xcp.nix
+++ nixpkgs/xcp.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "xcp";
-  version = "0.7.3";
+  version = "0.9.0";
 
   src = fetchFromGitHub {
     owner = "tarka";
     repo = pname;
     rev = "v${version}";
-    sha256 = "025h7439yl12aisd5aba7v2v1axhifgmr3cwcmx7n721xc7zpgvi";
+    sha256 = "0ia1rj05fkvb7bgycx41c7aa3cki8j0xwn25kz47dswgpfl7a2rb";
   };
 
-  cargoSha256 = "0ayamxkrkiyqnnhir2zms0bb5kdgg9vrx7x0n75i8hwb55fjlfcl";
+  cargoSha256 = "0kkhrwydnpxnqy6j2aw5fz4azbx0r9zam94rx60n2zlf0i2rfly0";
 
   doCheck = false;
 }
