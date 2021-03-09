93708f700e5d9f1bb57ab6f24ea00cd406e3da65
youxkei <youxkei@gmail.com>
2021-03-09 22:23:30 +0900

    [jack] Refactor start_jack.sh

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -7,7 +7,7 @@ jack_disconnect system:capture_1 'PulseAudio JACK Source:front-left'
 jack_disconnect system:capture_2 'PulseAudio JACK Source:front-right'
 
 pacmd unload-module module-virtual-surround-sink
-pacmd load-module module-virtual-surround-sink sink_name=surround sink_properties=device.description=surround master=jack_out hrir=$HOME/repo/dotfiles/jack/hrir-1003.wav
+pacmd load-module module-virtual-surround-sink sink_name=surround sink_properties=device.description=surround master=jack_out hrir="${HOME}/repo/dotfiles/jack/hrir-1003.wav"
 
 pacmd load-module module-jack-sink client_name=PulseAudio_JACK_Sink_2 connect=no
 pacmd load-module module-jack-source client_name=PulseAudio_JACK_Source_2 connect=no
