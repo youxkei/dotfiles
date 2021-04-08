68bb34f3bd0fdd1350466b8a0de2ed0980a46f14
youxkei <youxkei@gmail.com>
2021-04-08 23:44:02 +0900

    [pulseaudio] Disable switch on port available

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -6,6 +6,8 @@ jack_disconnect 'PulseAudio JACK Sink:front-right' system:playback_2
 jack_disconnect system:capture_1 'PulseAudio JACK Source:front-left'
 jack_disconnect system:capture_2 'PulseAudio JACK Source:front-right'
 
+pacmd unload-module module-switch-on-port-available
+
 pacmd unload-module module-virtual-surround-sink
 pacmd load-module module-virtual-surround-sink sink_name=surround sink_properties=device.description=surround master=jack_out hrir="${HOME}/repo/dotfiles/jack/hrir-1003.wav"
 
