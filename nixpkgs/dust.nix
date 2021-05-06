e0fbe9876cf49775a815e0e14642768ea7944b2d
youxkei <youxkei@gmail.com>
2021-05-06 23:15:03 +0900

    [nix] Disable dust tests

--- nixpkgs/dust.nix
+++ nixpkgs/dust.nix
@@ -11,5 +11,7 @@ rustPlatform.buildRustPackage rec {
     sha256 = "15737dv26r73j0xg071v6dbkncvggp3kwx1gqym5qmrh4zb9l097";
   };
 
+  doCheck = false;
+
   cargoSha256 = "0ciwaynpyy8qg08zqw977qzb31nqvkkhv5swngigdq6s5b9l79m3";
 }
