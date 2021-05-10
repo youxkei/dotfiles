ab960629535f0128ccfd8e0ddaaef7a094842086
youxkei <youxkei@gmail.com>
2021-05-10 20:24:26 +0900

    [jack][pipewire] Introduce pipewire

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
-jack_control start
-sleep 1s
-jack_disconnect 'PulseAudio JACK Sink:front-left' system:playback_1
-jack_disconnect 'PulseAudio JACK Sink:front-right' system:playback_2
-jack_disconnect system:capture_1 'PulseAudio JACK Source:front-left'
-jack_disconnect system:capture_2 'PulseAudio JACK Source:front-right'
+pactl unload-module module-null-sink
 
-pacmd unload-module module-switch-on-port-available
+pactl load-module module-null-sink sink_name=sink object.linger=1 media.class=Audio/Sink channel_map=FL,FR
+pactl load-module module-null-sink sink_name=source object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
 
-pacmd unload-module module-virtual-surround-sink
-pacmd load-module module-virtual-surround-sink sink_name=surround sink_properties=device.description=surround master=jack_out hrir="${HOME}/repo/dotfiles/jack/hrir-1003.wav"
+pactl set-default-sink sink
+pactl set-default-source source
 
-pacmd load-module module-jack-sink client_name=PulseAudio_JACK_Sink_2 connect=no
-pacmd load-module module-jack-source client_name=PulseAudio_JACK_Source_2 connect=no
-
-pacmd set-default-sink surround
-pacmd set-default-source jack_in
-
-carla ~/repo/dotfiles/jack/patch.carxp
+pw-jack carla ~/repo/dotfiles/jack/patch.carxp
