2550f86223f852305d825eaae82de8716f0fe2d0
youxkei <youxkei@gmail.com>
2021-05-22 21:22:37 +0900

    [jack] Introduce direct_sink

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+set -x
 pactl unload-module module-null-sink
 
 pactl load-module module-null-sink sink_name=sink object.linger=1 media.class=Audio/Sink channel_map=FL,FR
+pactl load-module module-null-sink sink_name=direct_sink object.linger=1 media.class=Audio/Sink channel_map=FL,FR
 pactl load-module module-null-sink sink_name=source object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
 
 pactl set-default-sink sink
