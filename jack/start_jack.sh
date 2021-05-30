d918d18770c1106f900839b66bfa9414a5d759bb
youxkei <youxkei@gmail.com>
2021-05-31 02:30:10 +0900

    [jack] Use own built carla

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -2,11 +2,11 @@
 set -x
 pactl unload-module module-null-sink
 
-pactl load-module module-null-sink sink_name=sink object.linger=1 media.class=Audio/Sink channel_map=FL,FR
-pactl load-module module-null-sink sink_name=direct_sink object.linger=1 media.class=Audio/Sink channel_map=FL,FR
-pactl load-module module-null-sink sink_name=source object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
+pactl load-module module-null-sink sink_name=sink        object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
+pactl load-module module-null-sink sink_name=direct_sink object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
+pactl load-module module-null-sink sink_name=source      object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
 
 pactl set-default-sink sink
 pactl set-default-source source
 
-pw-jack carla ~/repo/dotfiles/jack/patch.carxp
+pw-jack ~/repo/Carla/source/frontend/carla ~/repo/dotfiles/jack/patch.carxp
