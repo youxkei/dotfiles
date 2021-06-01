355241395488b9c19afd37d717b972523e9c5d23
youxkei <youxkei@gmail.com>
2021-06-01 22:38:10 +0900

    [jack] Add sink for tts

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -4,6 +4,7 @@ pactl unload-module module-null-sink
 
 pactl load-module module-null-sink sink_name=sink        object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
 pactl load-module module-null-sink sink_name=direct_sink object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
+pactl load-module module-null-sink sink_name=tts_sink    object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
 pactl load-module module-null-sink sink_name=source      object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
 
 pactl set-default-sink sink
