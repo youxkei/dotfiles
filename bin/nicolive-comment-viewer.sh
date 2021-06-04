484e14e81d10bf7736e817a11be8deb4b60a6f55
youxkei <youxkei@gmail.com>
2021-06-04 21:11:12 +0900

    [bin] Handle some commands in nicolive-comment-viewer.sh

--- bin/nicolive-comment-viewer.sh
+++ bin/nicolive-comment-viewer.sh
@@ -4,10 +4,28 @@ set -eu
 nicolive-comments -j -f "$@" \
     | tee \
         >(jq --unbuffered -r .chat.content \
-            | ruby -ple '
+            | ruby -r json -ple '
                 $stdout.sync = true
-                $_.gsub!(/[^\p{In_Hiragana}\p{In_Katakana}\p{In_CJK_Unified_Ideographs}\p{Number}\p{Letter}$＄%％&＆~\/〜\-=＝]/, " ")
-                $_.gsub!(/・| +/, " ")
+                _, command, body = /^\/(\w+)(?: (.+))?$/.match($_).to_a
+
+                if command then
+                    $_ = body
+
+                    case command
+                    when "nicoad" then
+                        $_ = JSON.parse(body)["message"]
+                    when "spi" then
+                        $_ = JSON.parse(body)
+                    when "info" then
+                        _, body = /^\d+ (.+)$/.match(body).to_a
+                        $_ = body
+                    when "clear" then
+                        $_ = ""
+                    end
+                else
+                    $_.gsub!(/[^\p{In_Hiragana}\p{In_Katakana}\p{In_CJK_Unified_Ideographs}\p{Number}\p{Letter}$＄%％&＆~\/〜\-=＝]/, " ")
+                    $_.gsub!(/・| +/, " ")
+                end
             ' \
             | rg -o --line-buffered '^.{0,30}' \
             | tts -l ja-JP --voice ja-JP-Wavenet-B -s 1.2 --pitch 2.5 -g 10.0 \
