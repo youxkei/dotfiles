8b76d336ad6068aafb6212ccc247baeca3e498e2
youxkei <youxkei@gmail.com>
2021-08-12 18:10:41 +0900

    [neovim] introduce nvim-toggleterm.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -417,6 +417,14 @@ require("packer").startup{
 
     use{"tversteeg/registers.nvim"}
 
+    use {"akinsho/nvim-toggleterm.lua", config = function()
+      require("toggleterm").setup{
+        size = 40,
+        open_mapping = "<c-t>",
+        direction = "float",
+      }
+    end}
+
     use{"elzr/vim-json", config = function()
       vim.g.vim_json_syntax_conceal = 0
     end}
