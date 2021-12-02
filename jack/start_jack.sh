2fec0cc0df87c2315fcacd2c27ef25f8c5457c92
youxkei <youxkei@gmail.com>
2021-12-02 21:40:05 +0900

    [i3][jack] introduce sink and source for discord

--- jack/start_jack.sh
+++ jack/start_jack.sh
@@ -6,9 +6,11 @@ pactl load-module module-null-sink sink_name=sink        object.linger=1 media.c
 pactl load-module module-null-sink sink_name=direct_sink object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
 pactl load-module module-null-sink sink_name=tts_sink    object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
 pactl load-module module-null-sink sink_name=stream_sink object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
+pactl load-module module-null-sink sink_name=discord_sink object.linger=1 media.class=Audio/Sink           channel_map=FL,FR
 
 pactl load-module module-null-sink sink_name=source        object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
 pactl load-module module-null-sink sink_name=stream_source object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
+pactl load-module module-null-sink sink_name=discord_source object.linger=1 media.class=Audio/Source/Virtual channel_map=FL,FR
 
 pactl set-default-sink sink
 pactl set-default-source source
