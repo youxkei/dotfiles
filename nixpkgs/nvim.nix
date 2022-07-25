3fff66d5b015afea195b9b376a241d669be6806e
youxkei <youxkei@gmail.com>
2022-07-26 01:57:29 +0900

    [nix] update neovim

--- nixpkgs/nvim.nix
+++ nixpkgs/nvim.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "nvim";
-  version = "v0.7.0";
+  version = "v0.7.2";
 
   src = fetchurl {
     url = "https://github.com/neovim/neovim/releases/download/${version}/nvim.appimage";
-    sha256 = "sha256:1nv1m9xf6avp4vfhjgpgh55crldh7jckscdr4vhhqkn5y7jal75c";
+    sha256 = "1qaqjr525dbj1icf0f82mf7qysrvv3m0c4sv32sx3dihqwhd1d9k";
   };
 
   unpackPhase = ":";
