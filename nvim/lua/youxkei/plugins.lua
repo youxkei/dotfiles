30c87c251d26f4c4e9f162b3e2b8d22638e8a810
youxkei <youxkei@gmail.com>
2023-01-14 11:47:56 +0900

    [neovim] adjust timeoutlen

--- nvim/lua/youxkei/plugins.lua
+++ nvim/lua/youxkei/plugins.lua
@@ -757,7 +757,7 @@ return {
 
   { "folke/which-key.nvim", config = function()
     vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    vim.o.timeoutlen = 1000
     require("which-key").setup {}
   end },
 
