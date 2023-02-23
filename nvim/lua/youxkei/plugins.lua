d425c8a6967632383d5620b961fdf5e1be9434b1
youxkei <youxkei@gmail.com>
2023-02-23 22:25:53 +0900

    [neovim] introduce nvim-snippy instead of luasnip

--- nvim/lua/youxkei/plugins.lua
+++ nvim/lua/youxkei/plugins.lua
@@ -414,20 +414,20 @@ return {
 
   { "hrsh7th/nvim-cmp",
     dependencies = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-buffer",
       "octaltree/cmp-look",
       "hrsh7th/cmp-cmdline",
+      "dcampos/nvim-snippy",
+      "dcampos/cmp-snippy",
     },
     config = function()
       local cmp = require("cmp")
       cmp.setup {
         snippet = {
           expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            require("snippy").expand_snippet(args.body)
           end
         },
         window = {
