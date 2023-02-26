dbb57a6aa6ab9f2c36b0e8756c0cf7e793cec4c8
youxkei <youxkei@gmail.com>
2023-02-26 14:51:42 +0900

    [nix][neovim] update neovim to v0.8.3

--- nixpkgs/nvim.nix
+++ nixpkgs/nvim.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "nvim";
-  version = "v0.8.1";
+  version = "v0.8.3";
 
   src = fetchurl {
     url = "https://github.com/neovim/neovim/releases/download/${version}/nvim.appimage";
-    hash = "sha256-JWWPPbWfrDmLwot/95hO0b7N+aPpiz5OImwjbDUQcFQ=";
+    hash = "sha256-Bfeiyxd/RQaIK3zYZ9giB8eQZuepVQ5O945urFN91Kg=";
   };
 
   unpackPhase = ":";
