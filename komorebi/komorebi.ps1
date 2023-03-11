25a5096a9a5042086a9b3d630f22b6b1e504a9cd
youxkei <youxkei@gmail.com>
2023-03-11 17:55:54 +0900

    [profile][komorebi][whkd] introduce whkd and use komorebi.ps1 instead of komorebi.ahk

---
+++ komorebi/komorebi.ps1
@@ -0,0 +1,28 @@
+stop-process -Name whkd
+start-process whkd -WindowStyle hidden
+
+. $PSScriptRoot\komorebi.generated.ps1
+
+$left_monitor = 2
+$center_monitor = 0
+$right_monitor = 1
+
+komorebic monitor-index-preference -- $center_monitor     0     0 3840 2160
+komorebic monitor-index-preference -- $right_monitor   3840     0 3840 2160
+komorebic monitor-index-preference -- $left_monitor   -2160 -1680 2160 3840
+
+komorebic ensure-named-workspaces $left_monitor   l0 l1 l2
+komorebic ensure-named-workspaces $center_monitor c0 c1 c2 c3 c4 c5
+komorebic ensure-named-workspaces $right_monitor  r0 r1 r2 r3 r4 r5
+
+komorebic identify-tray-application exe Carla.exe
+komorebic identify-tray-application exe qjackctl.exe
+komorebic identify-tray-application exe jackd.exe
+
+komorebic mouse-follows-focus enable
+komorebic focus-follows-mouse enable -i windows
+komorebic window-hiding-behaviour cloak
+komorebic cross-monitor-move-behaviour insert
+
+komorebic watch-configuration enable
+komorebic complete-configuration
