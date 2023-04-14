8f1764b21ff506864e873874b7031da2fd6b9be6
youxkei <youxkei@gmail.com>
2023-04-14 20:36:22 +0900

    [bin] adjust the waiting time between qjackctl and carla

--- bin/audio.ps1
+++ bin/audio.ps1
@@ -1,3 +1,3 @@
 Start-Process -FilePath "C:\Program Files\JACK2\qjackctl\qjackctl.exe" &
-Start-Sleep -s 20
+Start-Sleep -s 10
 Start-Process -FilePath "P:\software\Carla\Carla.exe" -ArgumentList "P:\carla\patch_windows.carxp" &
