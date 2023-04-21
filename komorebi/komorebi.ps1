1ff34535721cd5a43363a74ad1d284a75aa4a738
youxkei <youxkei@gmail.com>
2023-04-21 22:23:59 +0900

    [komorebi][whkd] add c6 and c7

--- komorebi/komorebi.ps1
+++ komorebi/komorebi.ps1
@@ -12,7 +12,7 @@ komorebic monitor-index-preference -- $right_monitor   3840     0 3840 2160
 komorebic monitor-index-preference -- $left_monitor   -2160 -1680 2160 3840
 
 komorebic ensure-named-workspaces $left_monitor   l0 l1 l2
-komorebic ensure-named-workspaces $center_monitor c0 c1 c2 c3 c4 c5
+komorebic ensure-named-workspaces $center_monitor c0 c1 c2 c3 c4 c5 c6 c7
 komorebic ensure-named-workspaces $right_monitor  r0 r1 r2 r3 r4 r5
 
 komorebic identify-tray-application exe Carla.exe
