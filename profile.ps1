49423d1695afd6a20b8e02873b4c4d6c29beada5
youxkei <youxkei@gmail.com>
2023-08-10 21:00:22 +0900

    [profile] add bash-like key handlers for powershell

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,9 @@
 $env:KOMOREBI_CONFIG_HOME = "$env:USERPROFILE\.config\komorebi"
+
+Set-PSReadLineKeyHandler -Key "Ctrl+w" -Function BackwardKillWord
+Set-PSReadLineKeyHandler -Key "Ctrl+u" -Function BackwardDeleteLine
+Set-PSReadLineKeyHandler -Key "Ctrl+k" -Function ForwardDeleteLine
+Set-PSReadLineKeyHandler -Key "Ctrl+a" -Function BeginningOfLine
+Set-PSReadLineKeyHandler -Key "Ctrl+e" -Function EndOfLine
+Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function PreviousHistory
+Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function NextHistory
