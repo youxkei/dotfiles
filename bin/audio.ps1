d82cacc2b0eeda5737cb51378869ff2693a9457c
youxkei <youxkei@gmail.com>
2023-08-10 20:58:23 +0900

    [bin] adjust waiting time for audio.ps1

--- bin/audio.ps1
+++ bin/audio.ps1
@@ -1,3 +1,3 @@
 Start-Process -FilePath "C:\Program Files\JACK2\qjackctl\qjackctl.exe" &
-Start-Sleep -s 10
+Start-Sleep -s 15
 Start-Process -FilePath "P:\software\Carla\Carla.exe" -ArgumentList "P:\carla\patch_windows.carxp" &
