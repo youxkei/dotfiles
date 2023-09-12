020cda871fe603ebea554567810d7a8d7507cdd7
youxkei <youxkei@gmail.com>
2023-09-12 22:02:02 +0900

    [bin] run script in job in audio.ps1

--- bin/audio.ps1
+++ bin/audio.ps1
@@ -1,19 +1,17 @@
-do
-{
-    Start-Sleep 5
-    $result = Get-PNPDevice | Where-Object {($_.Status -eq "ok") -and ($_.InstanceId -eq "USB\VID_1235&PID_8217\V275QW42602F0D")}
+Start-Job -ScriptBlock {
+    do {
+        Start-Sleep 5
+        $result = Get-PNPDevice | Where-Object {($_.Status -eq "ok") -and ($_.InstanceId -eq "USB\VID_1235&PID_8217\V275QW42602F0D")}
+    } while($result -eq $null)
+
+    $process = Start-Process -PassThru "C:\Program Files\JACK2\jackd.exe" -WindowStyle Hidden -ArgumentList '-S -X winmme -dportaudio -d "ASIO::ASIO Link Pro" -r48000 -p384'
+    $process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
+
+    do {
+        Start-Sleep 1
+        $result = Get-Process | Where-Object {$_.MainWindowTitle -match "^ASIO Link Pro"}
+    } while($result -eq $null)
+
+    $process = Start-Process -PassThru -FilePath "P:\software\Carla\Carla.exe" -ArgumentList "P:\carla\patch_windows.carxp"
+    $process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
 }
-while($result -eq $null)
-
-$process = Start-Process -PassThru "C:\Program Files\JACK2\jackd.exe" -WindowStyle Hidden -ArgumentList '-S -X winmme -dportaudio -d "ASIO::ASIO Link Pro" -r48000 -p384'
-$process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
-
-do
-{
-    Start-Sleep 1
-    $result = Get-Process | Where-Object {$_.MainWindowTitle -match "^ASIO Link Pro"}
-}
-while($result -eq $null)
-
-$process = Start-Process -PassThru -FilePath "P:\software\Carla\Carla.exe" -ArgumentList "P:\carla\patch_windows.carxp" -RedirectStandardOutput ".\out\NUL" -RedirectStandardError ".\err\NUL"
-$process.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
