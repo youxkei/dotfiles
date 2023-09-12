d7142041f01b6b056ea9806ec2bdca2911950e9d
youxkei <youxkei@gmail.com>
2023-09-12 21:19:43 +0900

    [bin] fix waiting of audio.ps1

--- bin/audio.ps1
+++ bin/audio.ps1
@@ -1,3 +1,17 @@
-Start-Process -FilePath "C:\Program Files\JACK2\qjackctl\qjackctl.exe" &
-Start-Sleep -s 15
+do
+{
+    Start-Sleep 5
+    $result = Get-PNPDevice | Where-Object {($_.Status -eq "ok") -and ($_.InstanceId -eq "USB\VID_1235&PID_8217\V275QW42602F0D")}
+}
+while($result -eq $null)
+
+Start-Process "C:\Program Files\JACK2\jackd.exe" -WindowStyle Hidden -ArgumentList '-S -X winmme -dportaudio -d "ASIO::ASIO Link Pro" -r48000 -p384' &
+
+do
+{
+    Start-Sleep 1
+    $result = Get-Process | Where-Object {$_.MainWindowTitle -match "^ASIO Link Pro"}
+}
+while($result -eq $null)
+
 Start-Process -FilePath "P:\software\Carla\Carla.exe" -ArgumentList "P:\carla\patch_windows.carxp" &
