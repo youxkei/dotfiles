b2d3b0a52156929e8d240da479ac4e981df6006d
youxkei <youxkei@gmail.com>
2023-10-26 21:37:54 +0900

    [bin] refactor audio.ps1

--- bin/audio.ps1
+++ bin/audio.ps1
@@ -4,7 +4,7 @@ Start-Job -ScriptBlock {
 
     do {
         Start-Sleep 5
-        $result = Get-PNPDevice | Where-Object {($_.Status -eq "ok") -and ($_.InstanceId -eq "USB\VID_1235&PID_8217\V275QW42602F0D")}
+        $result = Get-PNPDevice | ? {($_.Status -eq "ok") -and ($_.InstanceId -eq "USB\VID_1235&PID_8217\V275QW42602F0D")}
     } while($result -eq $null)
 
     $process = Start-Process -PassThru "C:\Program Files\JACK2\jackd.exe" -WindowStyle Hidden -ArgumentList '-S -X winmme -dportaudio -d "ASIO::ASIO Link Pro" -r48000 -p384'
@@ -12,7 +12,7 @@ Start-Job -ScriptBlock {
 
     do {
         Start-Sleep 10
-        $result = Get-Process | Where-Object {$_.MainWindowTitle -match "^ASIO Link Pro"}
+        $result = Get-Process | ? {$_.MainWindowTitle -match "^ASIO Link Pro"}
     } while($result -eq $null)
 
     $process = Start-Process -PassThru -FilePath "P:\software\Carla\Carla.exe" -ArgumentList "P:\carla\patch_windows.carxp"
