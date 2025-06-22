0945742b0bc09aff4078def43a00e178231f13af
youxkei <youxkei@gmail.com>
2025-06-22 17:52:02 +0900

    [neovim] remove avante.nvim superceded by claudecode.nvim

--- nvim/lua/youxkei/plugins.lua
+++ nvim/lua/youxkei/plugins.lua
@@ -1376,38 +1376,6 @@ return {
   },
 
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      provider = "gemini",
-      gemini = {
-        model = "gemini-2.0-flash-lite"
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-telescope/telescope.nvim",
-      "hrsh7th/nvim-cmp",
-      "nvim-tree/nvim-web-devicons",
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-
-  {
     "stevearc/quicker.nvim",
     event = "FileType qf",
     opts = {},
